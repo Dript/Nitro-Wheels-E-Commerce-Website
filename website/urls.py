2db1b2661f727d9c4e94d3ee0f0db14d09c95cd7
Shivam Kumar <46563640+shivamkr21@users.noreply.github.com>
2021-10-01 08:48:40 +0530

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -2,10 +2,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^nitro_wheels/',include('nitro_wheels.urls')),
+	
+	url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+	url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    	url(r'^admin/', admin.site.urls),
+    	url(r'^nitro_wheels/',include('nitro_wheels.urls')),
 ]
 
 
